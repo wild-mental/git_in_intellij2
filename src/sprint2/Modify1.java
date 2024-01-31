a690c22c4e48a40f9e479248ed132f7e4f0d8d09
wild-mental <wild.mental@kidsnote.com>
2024-01-31 13:57:31 +0900

    Revert "Merge pull request #3 from wild-mental/user1_branch2"
    This reverts commit 50096e5d7944824bdeb39a84c55ceb34215cefd2, reversing
    changes made to f08ffdd08d58a45588f9871faadac3c134d6e2cc.

--- src/sprint2/Modify1.java
+++
@@ -1,7 +0,0 @@
-package sprint2;
-
-public class Modify1 {
-    public static void main(String[] args) {
-        // sp2 변경사항 1
-    }
-}
--- src/sprint2/Modify2.java
+++
@@ -1,7 +0,0 @@
-package sprint2;
-
-public class Modify2 {
-    public static void main(String[] args) {
-        // sp2 변경사항 2
-    }
-}
